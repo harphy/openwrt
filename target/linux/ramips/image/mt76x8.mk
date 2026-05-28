2714874565485f4ed264843d3ca0e8039ff15a8e
harphy <zhenhuibox@gmail.com>
2026-05-28 20:03:35 +0800

    ramips: add support for LibreLink AP1200

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -104,6 +104,21 @@ define Device/asus_rt-ac1200-v2
 endef
 TARGET_DEVICES += asus_rt-ac1200-v2
 
+define Device/librelink_ap1200
+  IMAGE_SIZE := 16064k
+  KERNEL_LOADADDR := 0x81000000
+  KERNEL := kernel-bin | append-dtb | relocate-kernel $$(loadaddr-y) | \
+	lzma | uImage lzma
+  KERNEL_INITRAMFS := $$(KERNEL)
+  DEVICE_VENDOR := LibreLink
+  DEVICE_MODEL := AP1200
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-kernel | pad-to $$$$(BLOCKSIZE) | \
+	append-rootfs | pad-rootfs
+  DEVICE_PACKAGES := kmod-mt7615e kmod-mt7663-firmware-ap swconfig
+endef
+TARGET_DEVICES += librelink_ap1200
+
 define Device/asus_rt-n10p-v3
   IMAGE_SIZE := 7872k
   DEVICE_VENDOR := ASUS
